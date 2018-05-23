ec757bf743a2deddc25e4466c3134fedf0f71f6c
shobanavv <shobanavv@gmail.com>
2018-05-23 16:28:56 -0700

    created .env file and .gitignore

---
+++ bookmarks/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from uuid import uuid4
+
+# Create your models here.
+class Bookmark(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+  url = models.URLField('URL', unique=True)
+  name = models.CharField(max_length=200)
+  notes = models.TextField(blank=True)
